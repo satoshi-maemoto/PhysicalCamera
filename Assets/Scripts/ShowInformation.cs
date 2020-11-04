484e062fc173f9ed5450b585e5f5271e7d2c16e1
satoshi-maemoto <peugeot.106.s16@gmail.com>
2020-11-04 11:12:10 +0900

    Initial commit.

---
+++ Assets/Scripts/ShowInformation.cs
@@ -0,0 +1,17 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ShowInformation : MonoBehaviour
+{
+    private TextMeshProUGUI information;
+    void Start()
+    {
+        this.information = this.GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.information.text = $"Sensor Size: {Camera.main.sensorSize.x}mm x {Camera.main.sensorSize.y}mm\nFocal Length: {Camera.main.focalLength}mm";
+    }
+}
